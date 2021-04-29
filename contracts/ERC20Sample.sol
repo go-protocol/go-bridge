ba1f391be9b5fbd273c37fc492737ee89f230bcd
Fankouzu <1046166@qq.com>
2021-04-29 16:45:14 +0800

    update

---
+++ contracts/ERC20Sample.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.7.0;
+
+abstract contract ERC20Template is IERC20 {
+
+    function mint(address account, uint256 amount) public{
+    }
+    function burn(address account , uint256 amount) public{
+    }
+    function redeem(address account, uint256 amount)public {
+    }
+    function issue(address account, uint256 amount) public {
+    }
+}
